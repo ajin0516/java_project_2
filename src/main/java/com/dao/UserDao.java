796e3de5f06f7e519263a9ada7b4fd06f47a7b3e
ajin0516 <dkwls7432@naver.com>
2022-10-26 01:31:22 +0900

    add, findById, deleteAll, count Test

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -11,13 +11,12 @@ import static java.lang.Class.forName;
 public class UserDao {
 
     UserDao userDao;
+
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Map<String ,String > env = System.getenv();
         Connection conn = null;
         PreparedStatement ps = null;
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(env.get("DB_HOST"), env.get("DB_USER"), env.get("DB_PASSWORD"));
+            conn = connectionMaker();
             ps = conn.prepareStatement("INSERT INTO lastusers(id, name, password) VALUES (?,?,?)");
             ps.setString(1, user.getId());
             ps.setString(2, user.getName());
@@ -25,69 +24,66 @@ public class UserDao {
 
             ps.executeUpdate();
 
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
-        }finally {
-            ps.close();
-            conn.close();
+        } finally {
         }
+        if (ps != null) {
+            ps.close();
+        } else if (conn != null)
+            conn.close();
     }
 
     // 타입을 User로 설정해서 값을 User객체에서 받기
     // id로 해당 행 찾기
     public User findById(String id) throws ClassNotFoundException, SQLException {
-        Map<String ,String > env = System.getenv();
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
         User user;
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-             conn = DriverManager.getConnection(env.get("DB_HOST"), env.get("DB_USER"), env.get("DB_PASSWORD"));
-             ps = conn.prepareStatement("SELECT id, name, password FROM lastusers WHERE id = ?");
+            conn = connectionMaker();
+            ps = conn.prepareStatement("SELECT id, name, password FROM lastusers WHERE id = ?");
             ps.setString(1, id);
-             rs = ps.executeQuery(); // 조회문
+            rs = ps.executeQuery(); // 조회문
             rs.next();
             user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
-        }finally {
-            if(rs != null){
+        } finally {
+            if (rs != null) {
                 rs.close();
             } else if (ps != null) {
                 ps.close();
-            }else if(conn != null)
-            conn.close();
+            } else if (conn != null)
+                conn.close();
         }
         return user;
     }
 
-    public List<User> findALl() throws ClassNotFoundException, SQLException {
+    public List<User> findAll() throws ClassNotFoundException, SQLException {
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
         List<User> userList = null;
-        Map<String ,String > env = System.getenv();
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-             conn = DriverManager.getConnection(env.get("DB_HOST"), env.get("DB_USER"), env.get("DB_PASSWORD"));
-             ps = conn.prepareStatement("SELECT * d FROM lastusers ORDER BY id");
+            conn = connectionMaker();
+            ps = conn.prepareStatement("SELECT * FROM lastusers");
 
-             rs = ps.executeQuery(); // 조회문
-
-            rs.next();
+            rs = ps.executeQuery(); // 조회문
+            User user;
             while (rs.next()) {
-                User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+                user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
                 userList.add(user);
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
-        }finally {
-            if(rs != null){
+        } finally {
+            if (rs != null) {
                 rs.close();
             } else if (ps != null) {
                 ps.close();
-            }else if(conn != null)
+            } else if (conn != null)
                 conn.close();
         }
         return userList;
@@ -96,18 +92,18 @@ public class UserDao {
     public void deleteAll() throws ClassNotFoundException, SQLException {
         Connection conn = null;
         PreparedStatement ps = null;
-        Map<String ,String > env = System.getenv();
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-             conn = DriverManager.getConnection(env.get("DB_HOST"), env.get("DB_USER"), env.get("DB_PASSWORD"));
-             ps = conn.prepareStatement("DELETE FROM lastusers");
+            conn = connectionMaker();
+            ps = conn.prepareStatement("DELETE FROM lastusers");
 
             ps.executeUpdate();
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
-        }finally {
-            if(ps != null)
-            ps.close();
+        } finally {
+            if (ps != null) {
+                ps.close();
+            } else if (conn != null)
+                conn.close();
         }
     }
 
@@ -115,30 +111,29 @@ public class UserDao {
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        Map<String ,String > env = System.getenv();
         int cnt = 0;
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(env.get("DB_HOST"), env.get("DB_USER"), env.get("DB_PASSWORD"));
+            conn = connectionMaker();
+            ps = conn.prepareStatement("select count(*) from lastusers");
             rs = ps.executeQuery(); // 조회문
             while (rs.next()) {
                 cnt = rs.getInt(1);
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
-        }finally {
-            if(rs != null){
+        } finally {
+            if (rs != null) {
                 rs.close();
             } else if (ps != null) {
                 ps.close();
-            }else if(conn != null)
+            } else if (conn != null)
                 conn.close();
         }
         return cnt;
     }
 
     public Connection connectionMaker() throws ClassNotFoundException, SQLException {
-        Map<String ,String > env = System.getenv();
+        Map<String, String> env = System.getenv();
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection conn = DriverManager.getConnection(env.get("DB_HOST"), env.get("DB_USER"), env.get("DB_PASSWORD"));
         return conn;
@@ -148,7 +143,7 @@ public class UserDao {
         UserDao userDao = new UserDao();
 //        userDao.add();
 //        User user = userDao.findById("2");
-        userDao.add(new User("4","rara","3435"));
+//        userDao.add(new User("4", "rara", "3435"));
 //        System.out.println(user.getId());
 //        System.out.println(user.getName());
 //        System.out.println(user.getPassword());
--- src/test/java/com/dao/UserDaoTest.java
+++ src/test/java/com/dao/UserDaoTest.java
@@ -4,22 +4,56 @@ import domain.User;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserDaoTest {
 
-    User user;
-
     @Test
     void addAndFind() throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
+        userDao.deleteAll();
         User user1 = new User("7","haha","1324");
         userDao.add(user1);
         User addFind = userDao.findById("7");
         assertEquals("haha",addFind.getName());
+
+    }
+    @Test
+    void deleteAll() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+        userDao.deleteAll();
+        User user2 = new User("11","yaya","2020");
+        userDao.add(user2);
+        assertEquals(1,userDao.count());
+    }
+    @Test
+    void count() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+        userDao.deleteAll();
+        User user1 = new User("1","aj","29");
+        User user2 = new User("2","sy","26");
+        userDao.add(user1);
+        userDao.add(user2);
+        assertEquals(2,userDao.count());
     }
 
+//    @Test
+//    void findAll() throws SQLException, ClassNotFoundException {
+//        UserDao userDao = new UserDao();
+//        userDao.deleteAll();
+//        List<User> userList = userDao.findAll();
+//        assertEquals(0, userList.size());
 
+//        User user1 = new User("1","hong"," 88");
+//        User user2 = new User("2","song"," 99");
+//        userDao.add(user1);
+//        userDao.add(user2);
+//        userList = userDao.findALl();
+//        assertEquals(2,userList.size());
+
+//    }
 
 }
